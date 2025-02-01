30a613c41b3be3a41825a9d0e0219278359a48cf
omkar7647 <omkarpatil7647@gmail.com>
2025-02-01 15:31:43 +0530

    first commit

---
+++ build/CMakeFiles/nish.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nish.dir/main.cpp.o"
+  "nish"
+  "nish.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nish.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/omkarpatil/helloworld
+# Build directory: /home/omkarpatil/helloworld/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
